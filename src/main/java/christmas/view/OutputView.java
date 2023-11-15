74d062a7576c11359c3d78b0a10b735773be3089
Park-HyeJeong <park061138@gmail.com>
2023-11-15 23:22:03 +0900

    feat: add feature to print the estimated payment amount after discount

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -49,4 +49,9 @@ public class OutputView {
             System.out.printf("-%,d원%n", totalBenefitAmount);
         }
     }
+
+    public void printExpectedPaymentAmount(int expectedPaymentAmount) {
+        System.out.println("\n<할인 후 예상 결제 금액>");
+        System.out.printf("%,d원%n", expectedPaymentAmount);
+    }
 }
