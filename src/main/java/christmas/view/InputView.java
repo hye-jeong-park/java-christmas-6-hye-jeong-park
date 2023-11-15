b479f8282560c04e4c8b33561af7cfc831f77231
Park-HyeJeong <park061138@gmail.com>
2023-11-15 22:34:36 +0900

    feat: add feature when the number of menus exceeds 20

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.errors.ErrorHandler;
 import christmas.model.EventType;
 import christmas.model.Menu;
+import christmas.model.MenuCategory;
 import christmas.model.MenuOrder;
 import christmas.model.MenuPrice;
 import java.util.ArrayList;
@@ -23,7 +24,9 @@ public class InputView {
                 if (visitDate < 1 || visitDate > 31) {
                     errorHandler.handleException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
                 }
+
                 break;
+
             } catch (NumberFormatException e) {
                 errorHandler.handleException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             } catch (IllegalArgumentException e) {
@@ -33,14 +36,14 @@ public class InputView {
         return visitDate;
     }
 
-    public int readIntegerInput() {
-        String input = Console.readLine();
-        return Integer.parseInt(input);
-    }
-
     public String readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Console.readLine();
+    }
+
+    public int readIntegerInput() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public List<MenuOrder> parseOrderInput(String orderInput, ErrorHandler errorHandler) {
@@ -48,6 +51,7 @@ public class InputView {
         List<MenuOrder> menuOrders = new ArrayList<>();
 
         Set<String> uniqueMenuNames = new HashSet<>();
+        int totalQuantity = 0;
         boolean hasDrink = false;
 
         while (true) {
@@ -79,11 +83,25 @@ public class InputView {
                     break;
                 }
 
+                // 총 주문 개수가 20개가 넘었는지 확인
+                if (totalQuantity + quantity > 20) {
+                    menuOrders.clear();
+                    errorHandler.handleException("[ERROR] 주문 개수가 20개를 초과하였습니다. 다시 입력해 주세요.");
+                    break;
+                }
+
                 // 주문 메뉴가 중복되지 않았는지 확인
                 if (!uniqueMenuNames.add(menuName)) {
                     menuOrders.clear();
                     errorHandler.handleException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
                     break;
+                }
+
+                totalQuantity += quantity;
+
+                if (MenuCategory.DRINK == MenuPrice.getPriceByName(menuName, errorHandler)
+                    .getCategory() && menuOrders.size() == 0) {
+                    hasDrink = true;
                 }
 
                 // 메뉴판에 있는 메뉴인지 확인
