a8c359900cd060c74a282fd228ec9a7aadd4ba68
Park-HyeJeong <park061138@gmail.com>
2023-11-15 23:26:55 +0900

    feat: add Application.java for complete feature integration

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,20 @@
 package christmas;
 
+import christmas.controller.DecemberEventPlanner;
+import christmas.controller.DecemberEventPlannerImpl;
 import christmas.errors.ErrorHandler;
+import christmas.model.BadgeType;
+import christmas.model.Discount;
+import christmas.model.EventResult;
+import christmas.model.Menu;
 import christmas.model.MenuOrder;
+import christmas.model.Order;
+import christmas.utils.OrderCalculator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+
 import java.util.List;
+
 
 public class Application {
 
@@ -27,6 +37,31 @@ public class Application {
                     break;
                 }
             }
+
+            Order order = new Order(menuOrders);
+
+            DecemberEventPlanner decemberEventPlanner = new DecemberEventPlannerImpl();
+            EventResult eventResult = decemberEventPlanner.calculateEventResult(order, visitDate);
+
+            outputView.printMenu(order.getMenuOrders());
+            int totalOrderAmount = OrderCalculator.calculateTotalOrderAmount(order);
+            outputView.printTotalOrderAmount(totalOrderAmount);
+
+            Menu giftMenu = eventResult.getGiftMenu();
+            outputView.printGiftMenu(giftMenu);
+
+            List<Discount> discounts = eventResult.getDiscounts();
+            outputView.printDiscounts(discounts);
+
+            int totalBenefitAmount = OrderCalculator.calculateTotalDiscountAmount(discounts);
+            outputView.printTotalBenefitAmount(totalBenefitAmount);
+
+            int expectedPaymentAmount = totalOrderAmount - eventResult.getTotalDiscountAmountExcludingGift();
+            outputView.printExpectedPaymentAmount(expectedPaymentAmount);
+
+            BadgeType badge = eventResult.getBadgeType();
+            outputView.printBadge(badge);
+
         } catch (IllegalArgumentException e) {
             errorHandler.handleException(e.getMessage());
         }
