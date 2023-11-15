3de7d3b5dfbe8daf253f6f6a774f45e296e55f08
Park-HyeJeong <park061138@gmail.com>
2023-11-15 22:10:12 +0900

    feat: add feature to enter menu orders

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,34 @@
 package christmas;
 
+import christmas.errors.ErrorHandler;
+import christmas.model.MenuOrder;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ErrorHandler errorHandler = new ErrorHandler(inputView);
+
+        outputView.printStartMessage();
+        int visitDate = inputView.readDate(errorHandler);
+
+        try {
+            String orderInput;
+            List<MenuOrder> menuOrders;
+
+            while (true) {
+                orderInput = inputView.readOrder();
+                menuOrders = inputView.parseOrderInput(orderInput, errorHandler);
+                if (!menuOrders.isEmpty()) {
+                    break;
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            errorHandler.handleException(e.getMessage());
+        }
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,6 +2,15 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.errors.ErrorHandler;
+import christmas.model.EventType;
+import christmas.model.Menu;
+import christmas.model.MenuCategory;
+import christmas.model.MenuOrder;
+import christmas.model.MenuPrice;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InputView {
 
@@ -28,5 +37,17 @@ public class InputView {
     public int readIntegerInput() {
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+
+    public String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return Console.readLine();
+    }
+
+    public List<MenuOrder> parseOrderInput(String orderInput, ErrorHandler errorHandler) {
+        String[] orderTokens = orderInput.split(",");
+        List<MenuOrder> menuOrders = new ArrayList<>();
+
+        return menuOrders;
     }
 }
