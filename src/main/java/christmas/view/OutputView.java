3e2224a893667e877c3370a86c2cbbeaf0b28148
Park-HyeJeong <park061138@gmail.com>
2023-11-15 23:17:42 +0900

    feat: add feature to print ordered menus

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,18 @@
+package christmas.view;
+
+import christmas.model.*;
+import java.util.List;
+
+public class OutputView {
+
+    public void printStartMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printMenu(List<MenuOrder> menuOrders) {
+        System.out.println("\n<주문 메뉴>");
+        for (MenuOrder menuOrder : menuOrders) {
+            System.out.printf("%s %d개%n", menuOrder.getMenu().getName(), menuOrder.getQuantity());
+        }
+    }
+}
